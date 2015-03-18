d4f1852cc9cecfa53408397160687af803bf2348
Osiris Arguello <osirisarguello@yahoo.com.ar>
2015-03-18 16:13:59 -0300

    A partir del test y limitandome solo a Rock, implemento el vs para cada caso. Acá estoy teniendo en cuenta que el equals que se usa en cada caso es el equals de Object, que compara instancias. Por eso, si devuelvo la misma instancia el assertEquals deberia devolver true y si devuelvo otra instancia, el assertEquals deberia devolver false.

--- rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
+++ rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
@@ -2,4 +2,16 @@ package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class Rock {
 
+	public Object vs(Scissors scissors) {
+		return this;
+	}
+
+	public Object vs(Paper paper) {
+		return paper;
+	}
+
+	public Object vs(Rock rock) {
+		return this;
+	}
+
 }
